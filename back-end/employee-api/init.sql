c788103957c1fdd1c3b940edd3b215cb93b304d9
Bruno Zarth <brunozarth@gmail.com>
2024-04-15 07:35:23 -0300

    Implements: SQL commands to create tables

---
+++ back-end/employee-api/init.sql
@@ -0,0 +1,39 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS employee (
+    id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    position VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    password_salt VARCHAR(255) NOT NULL,
+    adress VARCHAR(255),
+    nationality VARCHAR(255),
+    age INTEGER,
+    education_level INTEGER,
+    gender VARCHAR(255),
+    ethnicity VARCHAR(255),
+    lgbtqi BOOLEAN,
+    pcd BOOLEAN,
+    neurodiverse BOOLEAN,
+    low_income_background BOOLEAN,
+    work_model VARCHAR(255),
+    hire_date DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS message (
+    id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
+    message TEXT NOT NULL,
+    position VARCHAR(255),
+    nationality VARCHAR(255),
+    age INTEGER,
+    education_level INTEGER,
+    gender VARCHAR(255),
+    ethnicity VARCHAR(255),
+    lgbtqi BOOLEAN,
+    pcd BOOLEAN,
+    neurodiverse BOOLEAN,
+    low_income_background BOOLEAN,
+    work_model VARCHAR(255),
+    hire_date TIMESTAMP NOT NULL
+);
