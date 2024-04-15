b6676fc5767ec54f4ac3a0d7ce50c22663cd0fe1
Bruno Zarth <brunozarth@gmail.com>
2024-04-15 07:41:28 -0300

    Fix: route age is now dateBirth

--- back-end/employee-api/src/routes/employee.js
+++ back-end/employee-api/src/routes/employee.js
@@ -43,7 +43,7 @@ router.get("/getByAdress/:adress", tokenValidated, employeeController.getByAdres
 
 router.get("/getByNationality/:nationality", tokenValidated, employeeController.getByNationality);
 
-router.get("/getByAge/:age", tokenValidated, employeeController.getByAge);
+router.get("/getByBirthDate/:BirthDate", tokenValidated, employeeController.getByAge);
 
 router.get("/getByAgeRange/:minAge/:maxAge", tokenValidated, employeeController.getByAgeRange);
 
